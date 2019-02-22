7fe592ea0e97b546ff1fef7ffb36f4f4a12c92ba
Chris Werner Rau <CWRau@CWRCoding.com>
2019-02-22 10:11:00 +0100

    Fix script locations

--- HOME/.config/screenlayouts/autosetup.sh
+++ HOME/.config/screenlayouts/autosetup.sh
@@ -2,10 +2,10 @@
 
 if xrandr | grep -Pzo 'HDMI1 .+\n(\s+.+\n)+' | grep -Pzo '\d+x1080 .+\n'
 then
-    $HOME/.screenlayout/1080multi.sh $(xrandr | grep -Pzo 'HDMI1 .+\n(\s+.+\n)+' | grep -Pzo '\d+x1080 .+\n' | sed -r 's#([0-9]+)x1080.+$#\1#g' | sort -h | tail -1)
+    $(dirname $(realpath $0))/1080multi.sh $(xrandr | grep -Pzo 'HDMI1 .+\n(\s+.+\n)+' | grep -Pzo '\d+x1080 .+\n' | sed -r 's#([0-9]+)x1080.+$#\1#g' | sort -h | tail -1)
 elif [ "$(xrandr | grep ' connected ' | wc -l)" -eq 1 ]
 then
-    $HOME/.screenlayout/laptop.sh
+    $(dirname $(realpath $0))/laptop.sh
 fi
 
 feh --bg-fill $HOME/.config/background.jpg
