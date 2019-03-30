6d098560cd17af9c90c6dd552bbaa84d72e1d41a
Chris Werner Rau <CWRau@CWRCoding.com>
2019-03-30 15:10:21 +0100

    Add yubikey safety toggle

--- HOME/.config/i3status/custom.d/public/safety_click_1.sh
+++ HOME/.config/i3status/custom.d/public/safety_click_1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -ex -o pipefail
-if rg '^#' /etc/udev/rules.d/20-yubikey.rules &> /dev/null
+if rg -v '^#' /etc/udev/rules.d/20-yubikey.rules &> /dev/null
 then
   sudo sed -i -r 's/^/#/g' /etc/udev/rules.d/20-yubikey.rules
   sudo udevadm control --reload-rules
