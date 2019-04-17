0f1926c4fe54fad7acf7d183341cdac7b42b3a7e
Chris Werner Rau <CWRau@CWRCoding.com>
2019-04-17 11:25:06 +0200

    Update!!

--- HOME/.config/i3status/custom.d/public/config-changes.sh
+++ HOME/.config/i3status/custom.d/public/config-changes.sh
@@ -2,10 +2,29 @@
 
 set -ex -o pipefail
 
-num=$(cd $HOME/projects/linux-config &> /dev/null && git ls-files -m | wc -l)
+unadded=$(cd $HOME/projects/linux-config &> /dev/null && git diff --name-only | wc -l)
+uncommitted=$(cd $HOME/projects/linux-config &> /dev/null && git diff --cached --name-only | wc -l)
+unpushed=$(cd $HOME/projects/linux-config &> /dev/null && git ls-files -m | wc -l)
 
-if [[ ${num:-0} -gt 0 ]]
+if [[ ${unadded:-0} -gt 0 ]]
 then
-    echo "$num Configs changed"
-    echo "$color_bad"
+    echo -n "$unadded file$([[ ${unadded:-0} -gt 1 ]] && echo s) changed"
+fi
+
+if [[ ${uncommitted:-0} -gt 0 ]]
+then
+    [[ ${unadded:-0} -gt 0 ]] && echo -n ', '
+    echo -n "$uncommitted change$([[ ${uncommitted:-0} -gt 1 ]] && echo s) not committed"
+fi
+
+if [[ ${unpushed:-0} -gt 0 ]]
+then
+    [[ ${uncommitted:-0} -gt 0 ]] && echo -n ', '
+    echo -n "$unpushed commit$([[ ${unpushed:-0} -gt 1 ]] && echo s) not pushed"
+fi
+
+if [[ ${unadded:-0} -gt 0 ]] || [[ ${uncommitted:-0} -gt 0 ]] || [[ ${unpushed:-0} -gt 0 ]]
+then
+  echo
+  echo "$color_bad"
 fi
