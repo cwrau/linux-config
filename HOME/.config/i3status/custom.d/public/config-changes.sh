65615428e00160bca63a952acf30cc2f8e6981c4
Chris Werner Rau <CWRau@CWRCoding.com>
2019-05-10 11:25:28 +0200

    Also display untracked files

--- HOME/.config/i3status/custom.d/public/config-changes.sh
+++ HOME/.config/i3status/custom.d/public/config-changes.sh
@@ -2,28 +2,37 @@
 
 set -ex -o pipefail
 
-unadded=$(cd $HOME/projects/linux-config &> /dev/null && git diff --name-only | wc -l)
-uncommitted=$(cd $HOME/projects/linux-config &> /dev/null && git diff --cached --name-only | wc -l)
-unpushed=$(cd $HOME/projects/linux-config &> /dev/null && git log --branches --not --remotes --format=%H | wc -l)
+cd $HOME/projects/linux-config &> /dev/null
+
+untracked=$(git ls-files -o --exclude-standard | wc -l)
+unadded=$(git diff --name-only | wc -l)
+uncommitted=$(git diff --cached --name-only | wc -l)
+unpushed=$(git log --branches --not --remotes --format=%H | wc -l)
+
+if [[ ${untracked:-0} -gt 0 ]]
+then
+    echo -n "$untracked file$([[ ${untracked} -gt 1 ]] && echo s) untracked"
+fi
 
 if [[ ${unadded:-0} -gt 0 ]]
 then
-    echo -n "$unadded file$([[ ${unadded:-0} -gt 1 ]] && echo s) changed"
+    [[ ${untracked:-0} -gt 0 ]] && echo -n ', '
+    echo -n "$unadded file$([[ ${unadded} -gt 1 ]] && echo s) changed"
 fi
 
 if [[ ${uncommitted:-0} -gt 0 ]]
 then
-    [[ ${unadded:-0} -gt 0 ]] && echo -n ', '
-    echo -n "$uncommitted change$([[ ${uncommitted:-0} -gt 1 ]] && echo s) not committed"
+    [[ ${untracked:-0} -gt 0 ]] || [[ ${unadded:-0} -gt 0 ]] && echo -n ', '
+    echo -n "$uncommitted change$([[ ${uncommitted} -gt 1 ]] && echo s) not committed"
 fi
 
 if [[ ${unpushed:-0} -gt 0 ]]
 then
-    [[ ${uncommitted:-0} -gt 0 ]] || [[ ${unadded:-0} -gt 0 ]] && echo -n ', '
-    echo -n "$unpushed commit$([[ ${unpushed:-0} -gt 1 ]] && echo s) not pushed"
+    [[ ${untracked:-0} -gt 0 ]] || [[ ${uncommitted:-0} -gt 0 ]] || [[ ${unadded:-0} -gt 0 ]] && echo -n ', '
+    echo -n "$unpushed commit$([[ ${unpushed} -gt 1 ]] && echo s) not pushed"
 fi
 
-if [[ ${unadded:-0} -gt 0 ]] || [[ ${uncommitted:-0} -gt 0 ]] || [[ ${unpushed:-0} -gt 0 ]]
+if [[ ${untracked:-0} -gt 0 ]] || [[ ${unadded:-0} -gt 0 ]] || [[ ${uncommitted:-0} -gt 0 ]] || [[ ${unpushed:-0} -gt 0 ]]
 then
   echo
   echo "$color_bad"
