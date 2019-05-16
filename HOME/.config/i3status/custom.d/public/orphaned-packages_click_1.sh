b782caa6048634043d50238fae89b10f67655bc7
Chris Werner Rau <CWRau@CWRCoding.com>
2019-05-16 16:42:31 +0200

    Fix quoting

--- HOME/.config/i3status/custom.d/public/orphaned-packages_click_1.sh
+++ HOME/.config/i3status/custom.d/public/orphaned-packages_click_1.sh
@@ -3,8 +3,8 @@
 choice=$(echo -e 'Yes\nManual\nNo' | dmenu -p "Remove Orphaned packages?")
 if [[ ${choice} = "Yes" ]]
 then
-  i3-msg exec 'gnome-terminal -- sh -c "yay -Rns --noconfirm $(yay -Qtdq)"' &> /dev/null
+  i3-msg exec "gnome-terminal -- sh -c 'yay -Rns --noconfirm \$(yay -Qtdq)'" &> /dev/null
 elif [[ ${choice} = "Manual" ]]
 then
-  i3-msg exec 'gnome-terminal -- sh -c "yay -Rns $(yay -Qtdq)"' &> /dev/null
+  i3-msg exec "gnome-terminal -- sh -c 'yay -Rns \$(yay -Qtdq)'" &> /dev/null
 fi
--- HOME/.config/i3status/custom.d/public/updates_click_1.sh
+++ HOME/.config/i3status/custom.d/public/updates_click_1.sh
@@ -3,8 +3,8 @@
 choice=$(echo -e 'Yes\nManual\nNo' | dmenu -p "Update packages?")
 if [[ ${choice} = "Yes" ]]
 then
-  i3-msg exec 'gnome-terminal -- sh -c "yay -Syu --noconfirm --removemake"' &> /dev/null
+  i3-msg exec "gnome-terminal -- sh -c 'yay -Syu --noconfirm --removemake'" &> /dev/null
 elif [[ ${choice} = "Manual" ]]
 then
-  i3-msg exec 'gnome-terminal -- sh -c "yay -Syu"' &> /dev/null
+  i3-msg exec "gnome-terminal -- sh -c 'yay -Syu'" &> /dev/null
 fi
