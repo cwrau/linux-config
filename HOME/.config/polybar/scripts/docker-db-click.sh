a7bd8e47ceeac1b5b2085aa0ffbfe4f421a17817
Chris Werner Rau <CWRau@CWRCoding.com>
2020-05-14 18:56:01 +0200

    Fix docker inspect when there is an image called db

--- HOME/.config/polybar/scripts/docker-db-click.sh
+++ HOME/.config/polybar/scripts/docker-db-click.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if docker inspect db &> /dev/null; then
+if docker inspect --type container db &> /dev/null; then
   docker stop db
 else
   docker run --rm -it -d --network host --name db -v /tmp/docker-db:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=toor yobasystems/alpine-mariadb
