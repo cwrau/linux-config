e821d8fd87c5e7fe24a3b1904d4db4a244e569dc
Chris Werner Rau <CWRau@CWRCoding.com>
2020-05-29 14:45:19 +0200

    Formatting

--- HOME/.config/polybar/scripts/bluetooth.sh
+++ HOME/.config/polybar/scripts/bluetooth.sh
@@ -6,8 +6,7 @@ pactl set-card-profile bluez_card.04_5D_4B_98_04_86 a2dp_sink &> /dev/null
 if bluetoothctl show | grep -q "Powered: yes"; then
   if bluetoothctl info | grep -q 'Device'; then
     echo -n "%{F#2193ff} "
-    for mac in $(bluetoothctl paired-devices | awk '{print $2}')                                                                                                                                                                                                                  ─╯
-    do
+    for mac in $(bluetoothctl paired-devices | awk '{print $2}'); do
       bluetoothctl info $mac | grep -B 99 'Connected: yes' | grep Alias | cut -d ' ' -f 2-
     done | sort | paste -sd "," - | sed 's#,#, #g'
   else
