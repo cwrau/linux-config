514bfd25030a5483e5dbd7b8daf91303ceabed59
Chris Werner Rau <cwrau@cwrcoding.com>
2020-06-19 15:58:25 +0200

    Update

--- HOME/.config/i3/rc.sh
+++ HOME/.config/i3/rc.sh
@@ -9,19 +9,19 @@ sudo xinput set-prop 'DLL06E4:01 06CB:7A13 Touchpad' 285 1
 touchscreenId="$(xinput | grep Touchscreen | sed -r 's#^.+id=([0-9]+).+$#\1#g')"
 if [[ ! -z $touchscreenId ]]
 then
-    sudo xinput map-to-output $touchscreenId eDP1
+  xinput --map-to-output $(xinput | rg Touchscreen | sed -r 's#^.+id=([0-9]+).+$#\1#g') $(xrandr | rg eDP | sed -r 's#^([^ ]+).+$#\1#g')
 fi
 
-xinput --map-to-output $(xinput | rg Touchscreen | sed -r 's#^.+id=([0-9]+).+$#\1#g') $(xrandr | rg eDP | sed -r 's#^([^ ]+).+$#\1#g')
 
-xinput | rg Touchpad | sed -r 's#^.+id=([0-9]+).+$#\1#g' | xargs -t -n 1 -I {} sh -x -c "xinput set-prop {} \$(xinput list-props {} | grep 'Tapping Enabled' | grep -i -v default | sed -r 's#^.+\(([0-9]+)\).+\$#\1#g') 1"
+touchPad=$(xinput | rg Touchpad | sed -r 's#^.+id=([0-9]+).+$#\1#g')
+tappingProperty=$(xinput list-props $touchPad | grep 'Tapping Enabled' | grep -i -v default | sed -r 's#^.+\(([0-9]+)\).+$#\1#g')
+xinput set-prop $touchPad $tappingProperty 1
 
-xinput | rg Touchpad | sed -r 's#^.+id=([0-9]+).+$#\1#g' | xargs -t -n 1 -I {} sh -x -c "xinput set-prop {} \$(xinput list-props {} | grep 'Disable While Typing Enabled' | grep -i -v default | sed -r 's#^.+\(([0-9]+)\).+\$#\1#g') 0"
+typingProperty=$(xinput list-props $touchPad | grep 'Disable While Typing Enabled' | grep -i -v default | sed -r 's#^.+\(([0-9]+)\).+$#\1#g')
+
+xinput set-prop $touchPad $typingProperty 0
 xinput --map-to-output $(xinput | rg Touchscreen | sed -r 's#^.+id=([0-9]+).+$#\1#g') $(xrandr | rg eDP | sed -r 's#^([^ ]+).+$#\1#g')
 
 sudo powertop --auto-tune
-systemctl --user start xdg-user-dirs-update
 
-for s in blugon clipmenud discord dunst feh foldingathome gnome-polkit-agent google-chrome google-play-music-player kdeconnect-indicator networkmanager-applet picom premid slack steam telegram whatsapp xfce4-power-manager antimicroX updates.timer; do
-  systemctl --user start $s
-done
+systemctl --user start applications.target
