64ee38c2fb4a2488541fb0aaaff52d341287f2bb
Chris Werner Rau <cwrau@cwrcoding.com>
2020-08-31 18:46:49 +0200

    Commit configs

--- .config/i3/rc.sh
+++ .config/i3/rc.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 
-sudo xinput set-prop 'DLL06E4:01 06CB:7A13 Touchpad' 297 1, 0, 0
-sudo xinput set-prop 'DLL06E4:01 06CB:7A13 Touchpad' 304 0
-sudo xinput set-prop 'DLL06E4:01 06CB:7A13 Touchpad' 288 0
-sudo xinput set-prop 'DLL06E4:01 06CB:7A13 Touchpad' 284 1
+#sudo xinput set-prop 'DLL06E4:01 06CB:7A13 Touchpad' 297 1, 0, 0
+#sudo xinput set-prop 'DLL06E4:01 06CB:7A13 Touchpad' 304 0
+#sudo xinput set-prop 'DLL06E4:01 06CB:7A13 Touchpad' 288 0
+#sudo xinput set-prop 'DLL06E4:01 06CB:7A13 Touchpad' 284 1
+#
+#sudo xinput set-prop 'DLL06E4:01 06CB:7A13 Touchpad' 285 1
+#touchscreenId="$(xinput | grep Touchscreen | sed -r 's#^.+id=([0-9]+).+$#\1#g')"
+#if [[ ! -z $touchscreenId ]]
+#then
+#  xinput --map-to-output $(xinput | rg Touchscreen | sed -r 's#^.+id=([0-9]+).+$#\1#g') $(xrandr | rg eDP | sed -r 's#^([^ ]+).+$#\1#g')
+#fi
+#
+#
+#touchPad=$(xinput | rg Touchpad | sed -r 's#^.+id=([0-9]+).+$#\1#g')
+#tappingProperty=$(xinput list-props $touchPad | grep 'Tapping Enabled' | grep -i -v default | sed -r 's#^.+\(([0-9]+)\).+$#\1#g')
+#xinput set-prop $touchPad $tappingProperty 1
+#
+#typingProperty=$(xinput list-props $touchPad | grep 'Disable While Typing Enabled' | grep -i -v default | sed -r 's#^.+\(([0-9]+)\).+$#\1#g')
+#
+#xinput set-prop $touchPad $typingProperty 0
+#xinput --map-to-output $(xinput | rg Touchscreen | sed -r 's#^.+id=([0-9]+).+$#\1#g') $(xrandr | rg eDP | sed -r 's#^([^ ]+).+$#\1#g')
 
-sudo xinput set-prop 'DLL06E4:01 06CB:7A13 Touchpad' 285 1
-touchscreenId="$(xinput | grep Touchscreen | sed -r 's#^.+id=([0-9]+).+$#\1#g')"
-if [[ ! -z $touchscreenId ]]
-then
-  xinput --map-to-output $(xinput | rg Touchscreen | sed -r 's#^.+id=([0-9]+).+$#\1#g') $(xrandr | rg eDP | sed -r 's#^([^ ]+).+$#\1#g')
-fi
-
-
-touchPad=$(xinput | rg Touchpad | sed -r 's#^.+id=([0-9]+).+$#\1#g')
-tappingProperty=$(xinput list-props $touchPad | grep 'Tapping Enabled' | grep -i -v default | sed -r 's#^.+\(([0-9]+)\).+$#\1#g')
-xinput set-prop $touchPad $tappingProperty 1
-
-typingProperty=$(xinput list-props $touchPad | grep 'Disable While Typing Enabled' | grep -i -v default | sed -r 's#^.+\(([0-9]+)\).+$#\1#g')
-
-xinput set-prop $touchPad $typingProperty 0
-xinput --map-to-output $(xinput | rg Touchscreen | sed -r 's#^.+id=([0-9]+).+$#\1#g') $(xrandr | rg eDP | sed -r 's#^([^ ]+).+$#\1#g')
-
-sudo powertop --auto-tune
-
-systemctl --user start applications.target
+#sudo powertop --auto-tune
