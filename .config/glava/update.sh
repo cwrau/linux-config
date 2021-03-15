931c05342e658d182c8a631a84de3f860f18bc82
Chris Werner Rau <cwrau@cwrcoding.com>
2021-03-15 17:35:16 +0100

    Update

--- .config/glava/update.sh
+++ .config/glava/update.sh
@@ -8,6 +8,7 @@ function update() {
     glavaOutputId=$(pactl list source-outputs short | awk '{if ($3 == "'"$glavaId"'") print $1;}')
     echo "Moving $glavaOutputId to @DEFAULT_MONITOR@"
     pactl move-source-output $glavaOutputId @DEFAULT_MONITOR@
+    pactl set-source-output-volume $glavaOutputId 50%
   done
 }
 
