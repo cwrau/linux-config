1366bd42d1bf7af30f41867e6b7a8a40e6d9967b
Chris Werner Rau <cwrau@cwrau.info>
2023-01-27 10:30:03 +0100

    update

--- .config/xorg/config.sh
+++ .config/xorg/config.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -ex
+
 xinput list | grep touchpad | cut -d = -f 2 | awk NF=1 | xargs -i xinput set-prop {} 'libinput Click Method Enabled' 0 1
 xinput list | grep touchpad | cut -d = -f 2 | awk NF=1 | xargs -i xinput set-prop {} 'libinput Tapping Enabled' 1
 
